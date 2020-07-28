6b5ce4aeb8221e472312a9d2c10a92f6e12afd53
AlexanderSwift89 <alexander.lyapunov89@gmail.com>
2020-07-28 22:03:23 +0300

    The first commit of 2020-07-28

---
+++ lesson_5/homeWork_5.0.1/app/src/main/java/com/example/homework_501/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.homework_501;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mInputNote;
+    private Button mBtnSaveNote;
+    private static String NOTE_TEXT = "note_text";
+
+    private SharedPreferences myNoteSharedPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews(){
+        mInputNote = findViewById(R.id.inputNote);
+        mBtnSaveNote = findViewById(R.id.btnSaveNote);
+
+        myNoteSharedPref = getSharedPreferences("MyNote", MODE_PRIVATE);
+
+        mBtnSaveNote.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                SharedPreferences.Editor myEditor = myNoteSharedPref.edit();
+                String noteTxt = mInputNote.getText().toString();
+                myEditor.putString(NOTE_TEXT, noteTxt);
+                myEditor.apply();
+                Toast.makeText(MainActivity.this, "данные сохранены", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
