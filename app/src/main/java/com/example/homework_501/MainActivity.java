16c94f26b801115082935ade4041355708ac7c24
AlexanderSwift89 <alexander.lyapunov89@gmail.com>
2020-07-30 13:22:52 +0300

    The commit of 2020-07-30

--- app/src/main/java/com/example/homework_501/MainActivity.java
+++ app/src/main/java/com/example/homework_501/MainActivity.java
@@ -13,33 +13,42 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText mInputNote;
     private Button mBtnSaveNote;
-    private static String NOTE_TEXT = "note_text";
-
-    private SharedPreferences myNoteSharedPref;
+    private final static String NOTE_TEXT = "note_text";
+    private static final String SHARED_KEY_TEXT="shared_key_text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String saveText = getSavedText();
+        mInputNote.setText(saveText);
+
         initViews();
+
+        mBtnSaveNote.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String noteTxt = mInputNote.getText().toString();
+                saveText(noteTxt);
+            }
+        });
+    }
+
+    private String getSavedText(){
+        return getSharedPreferences(NOTE_TEXT, MODE_PRIVATE).getString(SHARED_KEY_TEXT," ");
+    }
+
+    private void saveText(String noteTxt){
+        SharedPreferences myEditor = getSharedPreferences(NOTE_TEXT, MODE_PRIVATE);
+        myEditor.edit()
+                .putString(SHARED_KEY_TEXT, noteTxt)
+                .apply();
+        Toast.makeText(MainActivity.this, "данные сохранены", Toast.LENGTH_LONG).show();
     }
 
     private void initViews(){
         mInputNote = findViewById(R.id.inputNote);
         mBtnSaveNote = findViewById(R.id.btnSaveNote);
-
-        myNoteSharedPref = getSharedPreferences("MyNote", MODE_PRIVATE);
-
-        mBtnSaveNote.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                SharedPreferences.Editor myEditor = myNoteSharedPref.edit();
-                String noteTxt = mInputNote.getText().toString();
-                myEditor.putString(NOTE_TEXT, noteTxt);
-                myEditor.apply();
-                Toast.makeText(MainActivity.this, "данные сохранены", Toast.LENGTH_LONG).show();
-            }
-        });
     }
 }
